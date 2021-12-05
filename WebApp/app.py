0384322daf18152de6e0cb263f52decb25c2bff2
matsmolinski <matsmolinski@gmail.com>
2021-12-05 20:48:59 +0100

    important changes

--- WebApp/app.py
+++ WebApp/app.py
@@ -1,11 +1,9 @@
 from flask import Flask
 
-from WebApp.app.rest.index import index_blueprint
+from source.rest.index import index_blueprint
 
 
-if __name__ == "__main__":
-    app = Flask(__name__)
-    app.register_blueprint(
-        index_blueprint
-    )
-    app.run(host="localhost", port=5005)
+app = Flask(__name__)
+app.register_blueprint(
+    index_blueprint
+)
