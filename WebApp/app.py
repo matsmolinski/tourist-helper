00912d065cc898d0ae10597a8b15b8f94a7e2235
Patryk Zaniewski <kajgore@vp.pl>
2022-01-08 22:55:33 +0100

    env fix

--- WebApp/app.py
+++ WebApp/app.py
@@ -1,5 +1,3 @@
-from pathlib import Path
-
 from flask import Flask
 from dotenv import load_dotenv
 
@@ -7,8 +5,7 @@ from source.rest.index_controller import index_blueprint
 from source.rest.login_controller import login_blueprint
 from source.rest.translations_controller import translations_blueprint
 
-dotenv_path = Path("setting.yaml")
-load_dotenv(dotenv_path=dotenv_path)
+load_dotenv()
 app = Flask(__name__)
 app.secret_key = "SECRET KEY IN PLAIN TEXT PLZ DONT STEAL"
 
